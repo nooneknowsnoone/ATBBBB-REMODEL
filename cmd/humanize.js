b80179851bbe649096cafbfda7d9bac846e5c6c7
unknown <kenshimadax@gmail.com>
2026-03-19 14:42:20 +0800

    Update humanize.js

--- cmd/humanize.js
+++ cmd/humanize.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 module.exports.config = {
   name: "humanize",
   version: "1.0",
-  hasPermssion: 2,
+  hasPermssion: 1,
   credits: "VincentSensei, RY",
   description: "Makes text sound more human-like and conversational",
   commandCategory: "utility",
